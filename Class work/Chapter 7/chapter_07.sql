371e5107689c28d1662e93dabfaac7fd7bb72855
Emmanuel Tovurawa <emmanuel.tovurawa@gmail.com>
2021-10-25 22:13:21 +0200

    fifth commit

---
+++ Class work/Chapter 7/chapter_07.sql
@@ -0,0 +1,87 @@
+-- Listing 7-1: Declaring a single-column natural key as primary key
+
+-- As a column constraint
+CREATE TABLE natural_key_example (
+    license_id varchar(10) CONSTRAINT license_key PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+
+-- Drop the table before trying again
+DROP TABLE natural_key_example;
+
+-- As a table constraint
+CREATE TABLE natural_key_example (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT license_key PRIMARY KEY (license_id)
+);
+
+-- Listing 7-2: Example of a primary key violation
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Sam', 'Tracy');
+
+-- Listing 7-3: Declaring a composite primary key as a natural key
+CREATE TABLE natural_key_composite_example (
+    student_id varchar(10),
+    school_day date,
+    present boolean,
+    CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
+);
+
+-- Listing 7-4: Example of a composite primary key violation
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '2017-01-22', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '2017-01-23', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '2017-01-23', 'N');
+
+-- Listing 7-5: Declaring a bigserial column as a surrogate key
+
+CREATE TABLE surrogate_key_example (
+    order_number bigserial,
+    product_name varchar(50),
+    order_date date,
+    CONSTRAINT order_key PRIMARY KEY (order_number)
+);
+
+INSERT INTO surrogate_key_example (product_name, order_date)
+VALUES ('Beachball Polish', '2015-03-17'),
+       ('Wrinkle De-Atomizer', '2017-05-22'),
+       ('Flux Capacitor', '1985-10-26');
+
+SELECT * FROM surrogate_key_example;
+
+-- Listing 7-6: A foreign key example
+
+CREATE TABLE licenses (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
+CREATE TABLE registrations (
+    registration_id varchar(10),
+    registration_date date,
+    license_id varchar(10) REFERENCES licenses (license_id),
+    CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A203391', '2017-03-17', 'T229901');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A75772', '2017-03-17', 'T000001');
+
