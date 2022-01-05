e24162727effcc9410142d6b36cc909060afe344
Emmanuel Tovurawa <emmanuel.tovurawa@gmail.com>
2022-01-05 22:54:40 +0200

    postgresql project

--- The Complete PythonPostgreSQL/A Movie Watchlist App with Python & SQL/database.py
+++ The Complete PythonPostgreSQL/A Movie Watchlist App with Python & SQL/database.py
@@ -2,16 +2,33 @@ import datetime
 import sqlite3
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY,
     title TEXT,
-    release_timestamp REAL,
-    watched INTEGER
+    release_timestamp REAL
 );"""
 
-INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY
+);"""
+
+CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched (
+    user_username TEXT,
+    movie_id INTEGER,
+    FOREIGN KEY(user_username) REFERENCES users(username),
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
+);"""
+
+INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?)"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
-SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
-SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+INSERT_USER = "INSERT INTO users (username) VALUES (?)"
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?)"
+SELECT_WATCHED_MOVIES = """SELECT movies.*
+FROM users
+JOIN watched ON users.username = watched.user_username
+JOIN movies ON watched.movie_id = movies.id
+WHERE users.username = ?;"""
+SEARCH_MOVIE = """SELECT * FROM movies WHERE title LIKE ?;"""
 
 connection = sqlite3.connect("data.db")
 
@@ -19,15 +36,17 @@ connection = sqlite3.connect("data.db")
 def create_tables():
     with connection:
         connection.execute(CREATE_MOVIES_TABLE)
+        connection.execute(CREATE_USERS_TABLE)
+        connection.execute(CREATE_WATCHED_TABLE)
 
 
-def add_movie(content, release_timestamp):
+def add_movie(title, release_timestamp):
     with connection:
-        connection.execute(INSERT_MOVIE, (content, release_timestamp))
+        connection.execute(INSERT_MOVIE, (title, release_timestamp))
 
 
 def get_movies(upcoming=False):
-    with sqlite3.connect("data.db") as connection:
+    with connection:
         cursor = connection.cursor()
         if upcoming:
             today_timestamp = datetime.datetime.today().timestamp()
@@ -37,13 +56,25 @@ def get_movies(upcoming=False):
         return cursor.fetchall()
 
 
-def watch_movie(movie_title):
+def add_user(username):
     with connection:
-        connection.execute(SET_MOVIE_WATCHED, (movie_title,))
+        connection.execute(INSERT_USER, (username,))
 
 
-def get_watched_movies():
+def watch_movie(username, movie_id):
+    with connection:
+        connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
+
+
+def get_watched_movies(username):
     with connection:
         cursor = connection.cursor()
-        cursor.execute(SELECT_WATCHED_MOVIES)
+        cursor.execute(SELECT_WATCHED_MOVIES, (username,))
+        return cursor.fetchall()
+
+
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIE, (f"%{search_term}%",))
         return cursor.fetchall()
